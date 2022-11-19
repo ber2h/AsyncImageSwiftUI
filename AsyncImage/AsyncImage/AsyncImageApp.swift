56b3c29c7958fbf1e6d9ba8c188d6a63c6f24f5a
Bertuğ Horoz <99143306+ber2h@users.noreply.github.com>
2022-11-19 21:59:14 +0300

    Add files via upload

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Bertuğ Horoz on 19.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
